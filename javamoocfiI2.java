b6105b317e29f2bea99080cda699b37031562d9b
Shine <shine21ahuja@gmail.com>
2026-07-16 12:24:06 +0530

    Day 1
    Day 1- Part 1, module 1, 2 and 3

---
+++ javamoocfiI2.java
@@ -0,0 +1,7 @@
+//program boilerplate
+public class javamoocfiI2{
+    public static void main(String[]args){
+        System.out.println("hello world");
+        System.out.println("to print - type system.out.println(\"text\")");
+    }
+}
