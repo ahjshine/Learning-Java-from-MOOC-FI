d13769c981b5f37e78649f95fd149dc71c53dca6
Shine <160705244+ahjshine@users.noreply.github.com>
2026-07-19 17:19:05 +0530

    deleting old files

--- javamoocfiI2.java
+++
@@ -1,7 +0,0 @@
-//program boilerplate
-public class javamoocfiI2{
-    public static void main(String[]args){
-        System.out.println("hello world");
-        System.out.println("to print - type system.out.println(\"text\")");
-    }
-}
